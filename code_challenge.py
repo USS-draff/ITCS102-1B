056019158890f4cdae722e2f215261abf2ce8af6
USS-draff <samalbyberania25@gmail.com>
2025-08-13 15:21:58 +0800

    Add files via upload

---
+++ code_challenge.py
@@ -0,0 +1,3 @@
+name = input("Name? ")
+
+print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t*\t\tHi\t\t\t\t\t\t*\n*\t\t\t\t\tSam\t\t\t\t\t*\n\t*\t\t\t\t\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")

005d8f05033ba29cd2d2f54ab48f5e585f3cc793
USS-draff <samalbyberania25@gmail.com>
2025-08-14 00:44:19 +0800

    Add files via upload

---
+++ code_challege2.py
@@ -0,0 +1,19 @@
+n1 = eval(input("The amount of deposit : "))
+a = n1 // 1000
+s = n1 // 500
+d = n1 // 200
+f = n1 // 100
+g = n1 // 50
+h = n1 // 20
+j = n1 // 10
+k = n1 // 5
+l = n1 // 1
+print("\nThe floor division of",n1,"is",a)
+print("The floor division of",n1,"is",s)
+print("The floor division of",n1,"is",d)
+print("The floor division of",n1,"is",f)
+print("The floor division of",n1,"is",g)
+print("The floor division of",n1,"is",h)
+print("The floor division of",n1,"is",j)
+print("The floor division of",n1,"is",k)
+print("The floor division of",n1,"is",l)

d841bd9f095b06c167fcb1dd287288519e087ecd
USS-draff <samalbyberania25@gmail.com>
2025-08-27 16:00:18 +0800

    Add files via upload

---
+++ Activity10.py
@@ -0,0 +1,11 @@
+username = "samsam"
+passsword = "samsam1"
+
+uname = input("username --> ")
+pword = input("password --> ")
+
+if ('username' == uname) and ('password' == pword):
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENIED")
+
---
+++ code_challege3.py
@@ -0,0 +1,9 @@
+a = input("What is your name? ") 
+s = input("How much the fare fee? ")
+d = input("Are you a student? ")
+
+if s == 'Yes':
+	print ("The 20% discount is applied")
+
+if s == 'No':
+	print ("The 20% discount will not be applied")

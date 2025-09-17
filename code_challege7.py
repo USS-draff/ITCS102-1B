484efe0f6b7614383a75399f1b24fa2fde4e1d6d
USS-draff <samalbyberania25@gmail.com>
2025-09-17 17:02:43 +0800

    Add files via upload

---
+++ code_challege7.py
@@ -0,0 +1,6 @@
+print("⌛COUNTDOWN TIMER SIMULATOR")
+s = eval(input("Enter the starting number for countdown:"))
+print("Countdown starts:")
+for x in range (s,  0, -1):
+    print("\n",x)
+print("lifoff!!!!!")
---
+++ code_challege9.py
@@ -0,0 +1,6 @@
+print("PARROT SIMMULATOR - THE ECHO CHAMBER🦜")
+s = input("What do you want your parrot to say?----->")
+h = eval(input("How many times should the parrot squawk it?----->"))
+print("LISTEN TO YOUR PARROT")
+for x in range (0, h, 1):
+    print(s)

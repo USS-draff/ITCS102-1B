41a599cecf333d5af4c744cdee83d3312ca663e2
USS-draff <samalbyberania25@gmail.com>
2025-09-17 17:06:42 +0800

    code_challege9.py

--- code_challege9.py
+++ code_challege9.py
@@ -3,4 +3,5 @@ s = input("What do you want your parrot to say?----->")
 h = eval(input("How many times should the parrot squawk it?----->"))
 print("LISTEN TO YOUR PARROT")
 for x in range (0, h, 1):
-    print(s)
+    print("Squawk!",s)
+

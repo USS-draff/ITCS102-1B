d6c75be63b4ef5f22b15ce3a79b498ffd99dfd3d
USS-draff <samalbyberania25@gmail.com>
2025-09-25 14:01:41 +0800

    Add files via upload

---
+++ Activity16.py
@@ -0,0 +1,5 @@
+f = 'Sam Alby'
+m = 'S.'
+l = 'Berania'
+
+print(f"Your name is? {f.upper()} {m.upper()} {l.upper()}")
---
+++ Activity17.py
@@ -0,0 +1,3 @@
+print("Hello, ",end="")
+print("good morning ",end="")
+print("do like to have some coffe?")

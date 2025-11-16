3afef71ed6f7e202334c494a5babc71351196687
USS-draff <samalbyberania25@gmail.com>
2025-11-16 21:27:48 +0800

    Add files via upload

---
+++ Activitity24_1.py
@@ -0,0 +1,5 @@
+def Goodperson(name,asnwer):
+    print(f"What is your {name}, can I ask some question? {asnwer}.....")
+
+def Whereyoulive(address):
+    print(f"Great if yes, tell me where you live {address}")
---
+++ Activity24.py
@@ -0,0 +1,8 @@
+from Activitity24_1 import Goodperson,Whereyoulive
+
+def Goodmorning():
+    print("Hello there")
+
+Goodmorning()
+Goodperson(f"sam","yes")
+Whereyoulive("Lucena")
